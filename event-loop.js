eab71e9b6cca9011585fd6a03fab67b41946fd0e
LeonardoMeneghini <87610559+LeonardoMeneghini@users.noreply.github.com>
2022-08-21 04:56:48 -0300

    Create event-loop.js

---
+++ event-loop.js
@@ -0,0 +1,13 @@
+//Exemplo do conceito de requisições assíncronas no node
+function eventLoop(){
+    var a = 1;
+    var b = 2;
+    var c = 3;
+    console.log(a);
+
+    setTimeout(() => {
+        console.log(b);
+    }, 1000);
+    console.log(c);
+}
+eventLoop();
